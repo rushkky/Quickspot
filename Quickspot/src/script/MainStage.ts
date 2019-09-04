1948af7b698521986a2a674644999f9b410c7514
linfeng01 <89313912@qq.com>
2019-09-04 11:54:20 +0800

    新建工程

---
+++ Quickspot/src/script/MainStage.ts
@@ -0,0 +1,12 @@
+import { ui } from "../ui/layaMaxUI";
+
+export default class MainStage extends ui.MainStageUI{
+
+    constructor() { super(); }
+    
+    onEnable(): void {
+    }
+
+    onDisable(): void {
+    }
+}
